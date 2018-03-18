7a17238af2d24861c31709a3385e095da565aa78
sergey-astapov <serg.astapov@gmail.com>
2018-03-19 01:03:25 +0800

    - cleanup

--- sign-rules-scala/src/main/scala/io/signrools/ops/DocumentExt.scala
+++ sign-rules-scala/src/main/scala/io/signrools/ops/DocumentExt.scala
@@ -6,17 +6,20 @@ import com.roundeights.hasher.Implicits._
 
 object DocumentExt {
 
+  private def hashFunc: Array[Byte] => Array[Byte] = a => a.sha512.hash.bytes
+
   implicit class DocumentOps(doc: Document) {
     def hash: Array[Byte] = {
       val list = List(s"${doc.id.value}_${doc.name}_${doc.fileName}".getBytes(), doc.content)
-        .map(ba => ba.sha512.hash.bytes) ++ doc.signatures.map(s => s.value)
+        .map(DocumentExt.hashFunc) ++ doc.signatures.map(s => s.value)
       val size = list.foldLeft(0)((total, array) => total + array.length)
-      list.foldLeft((new Array[Byte](size), 0)) {
+      val array = list.foldLeft((new Array[Byte](size), 0)) {
         (acc, array) => {
           Array.copy(array, 0, acc._1, acc._2, array.length)
           (acc._1, acc._2 + array.length)
         }
-      }._1.sha512.hash.bytes
+      }._1
+      hashFunc(array)
     }
 
     def sign(signer: Signer): Document =
